6ee24f594bc5a4ed3c6dcf2fec085effa76ef99f
yuganshgarg01 <yuganshgarg01@gmail.com>
2025-08-11 11:05:36 +0530

    Create Program10.cpp
    11 August 2025 Class

---
+++ Program10.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main() {
+    int size;
+    cout << "Enter array size: ";
+    cin >> size;
+
+    int* arr = (int*) malloc(size * sizeof(int));
+
+    for (int i = 0; i < size; i++) {
+        arr[i] = i * 2;
+    }
+
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+
+    free(arr); // free memory
+
+    return 0;
+}
