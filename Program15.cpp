119006bf541bbcdd48d75068f38b8c211bd0c0fa
yuganshgarg01 <yuganshgarg01@gmail.com>
2025-08-26 12:16:23 +0530

    Update Program15.cpp 25/8/25

--- Program15.cpp
+++ Program15.cpp
@@ -1,38 +1,79 @@
-//Implementing a linear Queue using Array
-#include<iostream>
+// Implementing a linear Queue using Array
+#include <iostream>
 using namespace std;
+
 const int SIZE = 5;
 int Que[SIZE];
 int Front = 0;
 int Rear = -1;
+
 void display();
 void Enq(int element);
 void Deq();
+
 int main()
-{Enq(12);
-Enq(5);
-Enq(7);
-display();
-Enq(9);
-Enq(3);
-display();
-Enq(23);
-Deq();
-display();
-return 0;}
+{
+    Enq(12);
+    Enq(5);
+    Enq(7);
+    display();
+
+    Enq(9);
+    Enq(3);
+    display();
+
+    Enq(23);   // should say queue full
+    Deq();
+    display();
+
+    Deq();
+    Deq();
+    Deq();
+    Deq();     // should say queue empty
+    display();
+    Enq(85);
+    Enq(15);
+    display();
+
+    return 0;
+}
+
 void display()
-{cout<<"\n Elements in Queue are :";
-	for(int i=Front;i<=Rear;i++)
-		cout<<Que[i]<<" ";
-cout<<"\n\n";}
+{
+    if (Front > Rear)
+    {
+        cout << "\nQueue is Empty\n\n";
+        return;
+    }
+
+    cout << "\nElements in Queue are : ";
+    for (int i = Front; i <= Rear; i++)
+        cout << Que[i] << " ";
+    cout << "\n\n";
+}
+
 void Enq(int element)
-{if(Rear == SIZE -1)
-	cout<<"\nQueue Full";
-else
-{Rear++;
-	Que[Rear]=element;}}
+{
+    if (Rear == SIZE - 1)
+        cout << "\nQueue Full\n";
+    else
+    {
+        Rear++;
+        Que[Rear] = element;
+    }
+}
+
 void Deq()
-{if(Front ==0)
-	cout<<"\\n Queue Empty";
-else
-	Front ++;}
+{
+    if (Front > Rear)
+        cout << "\nQueue Empty\n";
+    else
+        Front++;
+
+    // Reset queue if all elements removed
+    if (Front > Rear)
+    {
+        Front = 0;
+        Rear = -1;
+    }
+}
