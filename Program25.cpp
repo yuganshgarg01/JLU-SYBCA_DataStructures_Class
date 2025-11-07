8dec7eb1c5ca5836915a6ab7af31a0592de38872
yuganshgarg01 <yuganshgarg01@gmail.com>
2025-11-07 10:38:12 +0530

    Bubble Sort

---
+++ Program25.cpp
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, j, temp;
+    
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+    
+    int arr[n];
+    printf("Enter %d elements:\n", n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    
+    // Bubble Sort
+    for (i = 0; i < n - 1; i++) {
+        for (j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // Swap elements
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+    
+    printf("Sorted array: ");
+    for (i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+    
+    return 0;
+}
